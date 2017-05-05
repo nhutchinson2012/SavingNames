c23cde2b6ed622c3a486e61aa67f177f2dfb442e
nhutchinson2012 <nathan.hutchinson@vacancy-filler.co.uk>
2017-05-05 14:54:13 +0100

    First Time Commit
    Commiting Folder First Time

---
+++ SaveTest/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace SaveTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public JsonResult FullName(string fullName)
+        {
+            //String Null Or Empty Return Fail
+            if(string.IsNullOrWhiteSpace(fullName))
+            {
+                return Json(new
+                {
+                    Success = false
+                }); 
+            }
+
+            //String Not Null Or Empty Try To Save To File
+            try
+            {
+                System.IO.StreamWriter file = new System.IO.StreamWriter("C:\\Users\\nathan.hutchinson\\Desktop\\SaveTest\\SaveTest\\Full Name Folder\\FullName.txt");
+                file.WriteLine(fullName);
+                file.Close();
+
+                return Json(new
+                {
+                    Success = true
+                }); 
+            }
+            catch
+            {
+                return Json(new
+                {
+                    Success = false
+                }); 
+            }
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ SaveTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SaveTest.Startup))]
+namespace SaveTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
